93319c06ba2a1ee865edfbab8d3df63fb81503ca
Kenneth Akpo <kennethakpo@KKs-MacBook-Pro.local>
2017-12-13 15:14:48 -0600

    commented out process variable not yet created in heroku

--- config/prod.js
+++ config/prod.js
@@ -2,18 +2,18 @@ module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
 
-  twitterconsumerKey: process.env.TWITTER_CONSUMER_KEY,
-  twitterconsumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+  twitterconsumerKey: "", //process.env.TWITTER_CONSUMER_KEY,
+  twitterconsumerSecret: "", //process.env.TWITTER_CONSUMER_SECRET,
 
-  facebookClientID: process.env.FACEBOOK_CLIENT_ID,
-  facebookClientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+  facebookClientID: "", //process.env.FACEBOOK_CLIENT_ID,
+  facebookClientSecret: "", //process.env.FACEBOOK_CLIENT_SECRET,
 
   mongoURI: process.env.MONGO_URI,
 
   sessionSecret: process.env.SESSION_SECRET,
 
-  AWSAccessKeyId: process.env.AWSAccessKeyId,
-  AWSSecretKey: process.env.AWSSecretKey,
+  AWSAccessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  AWSSecretKey: process.env.AWS_SECRET_KEY,
 
-  garageSaleStoragePath: process.env.garageSaleStoragePath
+  garageSaleStoragePath: process.env.GARAGE_SALE_STORAGE_PATH
 };
